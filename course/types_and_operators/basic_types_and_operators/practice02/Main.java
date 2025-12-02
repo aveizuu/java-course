d7d9e5fca8fae43432544a915c877ee153e2be22
aveizuu <yaroslav21560@mail.ru>
2025-12-02 22:06:30 +0300

    Задание 2 второго модуля выполнено

---
+++ course/types_and_operators/basic_types_and_operators/practice02/Main.java
@@ -0,0 +1,17 @@
+package types_and_operators.practice02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "\u0059" + // Y
+                "\u0061" + // a
+                "\u0072" + // r
+                "\u006F" + // o
+                "\u0073" + // s
+                "\u006C" + // l
+                "\u0061" + // a
+                "\u0076";  // v
+
+        System.out.println(name);
+    }
+}
